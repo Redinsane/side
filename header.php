6430a016df49333d6718861f8b9c747c31e2225d
Redinsane <redinsane@ukr.net>
2016-03-11 18:47:18 +0200

    Delete header.php

--- header.php
+++
@@ -1,8 +0,0 @@
-<head>
-	<meta charset="UTF-8">
-	<title>Доска плоска</title>
-	<div class="page-header">
-  <h1>Доска объявлений<br></h1>
-</div>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-</head>
