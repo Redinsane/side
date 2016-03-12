7b6f4efc0bcaff927b4983346f9fceb0dbf0fd83
Redinsane <redinsane@ukr.net>
2016-03-12 13:16:37 +0200

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -6,7 +6,16 @@
 </head>
 <body>
 	<h2>Добро пожаловать на сайт!</h2>
-	<a href="add.php">Добавить объявление</a>
+	<a href="add.php">Добавить объявление</a><br><br>
+	<?php
+	include ("init.php");
+	$result = mysqli_query($connection,"SELECT title, description, date_added, time FROM ads ORDER BY ID");
+	
+	while($row = mysqli_fetch_assoc($result)){
+		 echo '<h2><hr>'.$row['title'].'</h2>'.$row['description'].'<br><br>', $row['date_added'].' / ', $row['time'].'</hr>';
+	}
+
+	?>
 
 </body>
 </html>
