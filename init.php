4f180f365fe46a275c9f0e7262642dfd0932ea3f
Redinsane <redinsane@ukr.net>
2016-03-12 13:17:34 +0200

    Update init.php

--- init.php
+++ init.php
@@ -1,12 +1,6 @@
-<?php
-include 'homepage.php';
-$db = new PDO('mysql:host=localhost;dbname=bogdan', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
-$sql = 'SELECT * FROM ads';
-$result = $db->query($sql);
-	while ($ads = $result->fetch(PDO::FETCH_ASSOC)) {
-		echo "<h2>{$ads['id']} . {$ads['title']}</h2> {$ads['description']} <h3>{$ads['date_added']} / {$ads['time']}</h3><hr></hr><br>";
+$connection = new mysqli('localhost', 'root', '', 'bogdan');
+
+mysqli_query($connection," SET NAMES 'utf8'");
+if (!$connection) {
+	exit(mysqli_error());
 }
-
-
-
-	
